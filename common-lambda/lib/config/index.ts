853dc865e272170c8447ed5c9e48e0bbde8555ec
Gillis Van Ginderachter <ggindera@amazon.com>
2020-04-01 10:05:46 -0400

    Optimize config JSON example.

--- common-lambda/lib/config/index.ts
+++ common-lambda/lib/config/index.ts
@@ -1,66 +1,52 @@
 import * as t from 'io-ts';
-import { cidr, createEnumType, optional } from './types';
+import { cidr, region, availabilityZone, optional } from './types';
 import { PathReporter } from './reporter';
 import { NonEmptyString } from 'io-ts-types/lib/NonEmptyString';
 import { fromNullable } from 'io-ts-types/lib/fromNullable';
 import { isLeft } from 'fp-ts/lib/Either';
 
-export const Region = createEnumType(
-  [
-    // TODO Add more regions here
-    'ca-central-1',
-    'us-east-1',
-  ],
-  'Region',
-);
-
-export const AvailabilityZone = createEnumType(['a', 'b', 'c', 'd', 'e', 'f'], 'AvailabilityZone');
-
-export const InternetGatewayConfig = t.interface({
+const VirtualPrivateGatewayConfig = t.interface({
   // TODO
 });
 
-export const VirtualPrivateGatewayConfig = t.interface({
-  // TODO
-});
-
-export const PeeringConnectionConfig = t.interface({
+const PeeringConnectionConfig = t.interface({
   source: NonEmptyString,
   subnets: NonEmptyString,
   // TODO
 });
 
-export const NatGatewayConfig = t.interface({
+const NatGatewayConfig = t.interface({
   // TODO
 });
 
-export const SubnetRouteTableAssociationConfig = t.interface({
-  az: AvailabilityZone,
+const SubnetDefinitionConfig = t.interface({
+  az: availabilityZone,
   cidr: cidr,
   'route-table': NonEmptyString,
+  disabled: fromNullable(t.boolean, false),
 });
 
-export const SubnetConfig = t.interface({
+const SubnetConfig = t.interface({
   name: NonEmptyString,
   'share-to-ou-accounts': fromNullable(t.boolean, false),
-  'route-tables': t.array(SubnetRouteTableAssociationConfig),
+  'definitions': t.array(SubnetDefinitionConfig),
 });
 
-export const GatewayEndpointType = NonEmptyString; // TODO Define all endpoints here
+const GatewayEndpointType = NonEmptyString; // TODO Define all endpoints here
 
-export const RouteConfig = t.interface({
+const RouteConfig = t.interface({
   destination: t.unknown, // TODO Can be string or destination in another account
   target: NonEmptyString,
 });
 
-export const RouteTableConfig = t.interface({
+const RouteTableConfig = t.interface({
   name: NonEmptyString,
   routes: optional(t.array(RouteConfig)),
 });
 
-export const TransitGatewayAttachOption = NonEmptyString; // TODO Define all attach options here
+const TransitGatewayAttachOption = NonEmptyString; // TODO Define all attach options here
 
-export const TransitGatewayAttachConfig = t.interface({
+const TransitGatewayAttachConfig = t.interface({
   'associate-to-tgw': t.union([NonEmptyString, t.boolean]),
   account: optional(t.string),
   'associate-type': optional(t.literal('ATTACH')),
@@ -71,21 +57,21 @@ export const TransitGatewayAttachConfig = t.interface({
   options: optional(t.array(TransitGatewayAttachOption)),
 });
 
-export const InterfaceEndpointName = NonEmptyString; // TODO Define all endpoints here
+const InterfaceEndpointName = NonEmptyString; // TODO Define all endpoints here
 
-export const InterfaceEndpointConfig = t.interface({
+const InterfaceEndpointConfig = t.interface({
   subnet: NonEmptyString,
   endpoints: t.array(InterfaceEndpointName),
 });
 
-export const VpcConfigType = t.interface({
+const VpcConfigType = t.interface({
   deploy: optional(NonEmptyString),
   name: NonEmptyString,
   cidr: optional(cidr),
-  region: optional(Region),
+  region: optional(region),
   'flow-logs': fromNullable(t.boolean, false),
   'log-retention': optional(t.number),
-  igw: t.union([InternetGatewayConfig, t.boolean, t.undefined]),
+  igw: t.union([t.boolean, t.undefined]),
   vgw: t.union([VirtualPrivateGatewayConfig, t.boolean, t.undefined]),
   pcx: t.union([PeeringConnectionConfig, t.boolean, t.undefined]),
   natgw: t.union([NatGatewayConfig, t.boolean, t.undefined]),
@@ -96,16 +82,18 @@ export const VpcConfigType = t.interface({
   'interface-endpoints': t.union([InterfaceEndpointConfig, t.boolean, t.undefined]),
 });
 
-export const DeploymentFeature = NonEmptyString;
+export type VpcConfig = t.TypeOf<typeof VpcConfigType>;
 
-export const DeploymentConfig = t.interface({
+const DeploymentFeature = NonEmptyString;
+
+const DeploymentConfig = t.interface({
   name: optional(NonEmptyString),
   asn: optional(t.number),
   features: optional(t.record(DeploymentFeature, t.boolean)),
   'route-tables': optional(t.array(NonEmptyString)),
 });
 
-export const AccountConfigType = t.interface({
+const AccountConfigType = t.interface({
   'account-name': NonEmptyString,
   email: NonEmptyString,
   ou: NonEmptyString,
@@ -113,13 +101,13 @@ export const AccountConfigType = t.interface({
   deployments: t.record(NonEmptyString, DeploymentConfig),
 });
 
-export const MandatoryAccountConfigType = t.interface({
+const MandatoryAccountConfigType = t.interface({
   operations: AccountConfigType,
   'shared-network': AccountConfigType,
   master: AccountConfigType,
 });
 
-export const AcceleratorConfigType = t.interface({
+const AcceleratorConfigType = t.interface({
   'mandatory-account-configs': MandatoryAccountConfigType,
 });
 
