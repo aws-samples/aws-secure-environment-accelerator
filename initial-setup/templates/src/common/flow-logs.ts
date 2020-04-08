95ca02b5b250bd722f66b9f8602cd6e9c89bb260
Gillis Van Ginderachter <ggindera@amazon.com>
2020-04-08 15:25:03 -0400

    Fix deployment issue with flow logs

--- initial-setup/templates/src/common/flow-logs.ts
+++ initial-setup/templates/src/common/flow-logs.ts
@@ -1,9 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as iam from '@aws-cdk/aws-iam';
-
-// import { VpcConfig } from '@aws-pbmm/common-lambda/lib/config';
-import { Bucket } from '@aws-cdk/aws-s3';
+import * as s3 from '@aws-cdk/aws-s3';
 
 export interface FlowLogsProps extends cdk.StackProps {
   vpcId: string;
@@ -13,10 +11,7 @@ export class FlowLogs extends cdk.Construct {
   constructor(scope: cdk.Construct, id: string, props: FlowLogsProps) {
     super(scope, id);
 
-    //TODO Get the S3 bucket ARN
-    const s3Bucket = Bucket.fromBucketAttributes(this, 'TestBucket', {
-      bucketArn: 'arn:aws:s3:::vpcflowlog-bucket',
-    });
+    const s3Bucket = new s3.Bucket(this, 'VpcFlowLogs');
 
     const flowLogRole = new iam.Role(this, 'RoleFlowLogs', {
       roleName: 'AcceleratorVPCFlowLogsRole',
@@ -36,7 +31,7 @@ export class FlowLogs extends cdk.Construct {
       resourceId: props.vpcId!!,
       resourceType: 'VPC',
       trafficType: 'ALL',
-      logDestination: 'arn:aws:s3:::vpcflowlog-bucket', //TODO Get the S3 bucket ARN
+      logDestination: s3Bucket.bucketArn,
       logDestinationType: 's3',
     });
   }
