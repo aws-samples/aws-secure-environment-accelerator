e59f1bc9a38d7f1006a56d68a4ca5b3579a2b5d7
Gillis Van Ginderachter <ggindera@amazon.com>
2020-04-15 14:44:30 -0400

    Added initial password creation step

--- initial-setup/templates/src/perimeter/stack.ts
+++ initial-setup/templates/src/perimeter/stack.ts
@@ -18,7 +18,7 @@ export namespace Perimeter {
       const accountProps = props.accountConfig;
 
       // Create VPC, Subnets, RouteTables and Routes on Shared-Network Account
-      const vpcConfig = accountProps.vpc;
+      const vpcConfig = accountProps.vpc!;
       const vpc = new Vpc(this, 'vpc', vpcConfig);
 
       // Creating Interface endpoints
