0999fbcbb31287528ac5e71507a7d65d1042a3fb
Gillis Van Ginderachter <ggindera@amazon.com>
2020-04-24 17:11:49 -0400

    Add additional Landing Zone OU validation

---
+++ common-lambda/lib/util/arrays.ts
@@ -0,0 +1,33 @@
+function defaultEquality<T>(a: T, b: T) {
+  return a === b;
+}
+
+/**
+ * Returns true if both list `a` and `b` contain the same elements.
+ *
+ * @param a List to compare
+ * @param b List that is compared
+ * @param equality Function to check if elements equal
+ */
+export function arrayEqual<T>(
+  a: T[] | undefined,
+  b: T[] | undefined,
+  equality: (a: T, b: T) => boolean = defaultEquality,
+) {
+  if (a === undefined) {
+    return b === undefined;
+  } else if (b === undefined) {
+    return false;
+  } else if (a.length !== b.length) {
+    return false;
+  }
+
+  for (const ca of a) {
+    const found = b.find(cb => equality(ca, cb));
+    if (!found) {
+      console.debug(`Element ${ca} not found in array`);
+      return false;
+    }
+  }
+  return true;
+}
