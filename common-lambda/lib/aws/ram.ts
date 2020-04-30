fb43c933cfde908ae92e713fa4497cfb97b0e2be
Mani <62239997+manishri-amzn@users.noreply.github.com>
2020-04-30 14:03:49 +0530

    removed sdk code for createResourceShare (using cdk instead)

--- common-lambda/lib/aws/ram.ts
+++ common-lambda/lib/aws/ram.ts
@@ -1,5 +1,4 @@
 import * as aws from 'aws-sdk';
-import { CreateResourceShareRequest, CreateResourceShareResponse } from 'aws-sdk/clients/ram';
 
 export class RAM {
   private readonly client: aws.RAM;
@@ -12,13 +11,5 @@ export class RAM {
 
   async enableSharingWithAwsOrganization(): Promise<void> {
     await this.client.enableSharingWithAwsOrganization({}).promise();
-  }
-
-  /**
-   * to create resource share
-   * @param input
-   */
-  async createResourceShare(input: CreateResourceShareRequest): Promise<CreateResourceShareResponse> {
-    return this.client.createResourceShare(input).promise();
   }
 }
