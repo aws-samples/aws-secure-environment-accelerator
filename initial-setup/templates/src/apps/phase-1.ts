24e8aadf2e2a44c9b464664cfe5e5b863fb824bb
Mani <62239997+manishri-amzn@users.noreply.github.com>
2020-04-30 12:21:03 +0530

    updated the const references in phase-1

--- initial-setup/templates/src/apps/phase-1.ts
+++ initial-setup/templates/src/apps/phase-1.ts
@@ -10,11 +10,7 @@ import { loadStackOutputs } from '../utils/outputs';
 import { VpcStack } from '../common/vpc-stack';
 import { Vpc, VpcProps } from '../common/vpc';
 import { JsonOutputValue } from '../common/json-output';
-import {
-  OUTPUT_LOG_ARCHIVE_BUCKET_ARN,
-  OUTPUT_LOG_ARCHIVE_ENCRYPTION_KEY_ARN,
-  OUTPUT_LOG_ARCHIVE_ACCOUNT_ID,
-} from './phase-0';
+import * as outputKeys from '@aws-pbmm/common-outputs/lib/stack-output';
 
 process.on('unhandledRejection', (reason, _) => {
   console.error(reason);
@@ -58,9 +54,9 @@ async function main() {
 
   const globalOptions = acceleratorConfig['global-options'];
 
-  const logArchiveAccountId = getStackOutput(outputs, 'log-archive', OUTPUT_LOG_ARCHIVE_ACCOUNT_ID);
-  const logArchiveS3BucketArn = getStackOutput(outputs, 'log-archive', OUTPUT_LOG_ARCHIVE_BUCKET_ARN);
-  const logArchiveS3KmsKeyArn = getStackOutput(outputs, 'log-archive', OUTPUT_LOG_ARCHIVE_ENCRYPTION_KEY_ARN);
+  const logArchiveAccountId = getStackOutput(outputs, 'log-archive', outputKeys.OUTPUT_LOG_ARCHIVE_ACCOUNT_ID);
+  const logArchiveS3BucketArn = getStackOutput(outputs, 'log-archive', outputKeys.OUTPUT_LOG_ARCHIVE_BUCKET_ARN);
+  const logArchiveS3KmsKeyArn = getStackOutput(outputs, 'log-archive', outputKeys.OUTPUT_LOG_ARCHIVE_ENCRYPTION_KEY_ARN);
 
   const app = new cdk.App();
 
