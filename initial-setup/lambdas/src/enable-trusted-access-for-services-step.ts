98e409c4aae96ce685c27541cbfd4d8339244bdd
Mani <62239997+manishri-amzn@users.noreply.github.com>
2020-05-05 16:41:31 +0530

    enable trusted access for RAM, FMS, Access Analyzer and designate admin as security account for FMS and Access Analyzer

--- initial-setup/lambdas/src/enable-trusted-access-for-services-step.ts
+++ initial-setup/lambdas/src/enable-trusted-access-for-services-step.ts
@@ -1,4 +1,5 @@
 import { Organizations } from '@aws-pbmm/common-lambda/lib/aws/organizations';
+import { FMS } from '@aws-pbmm/common-lambda/lib/aws/fms';
 import { Account } from './load-accounts-step';
 
 interface EnableTrustedAccessForServicesInput {
@@ -11,27 +12,28 @@ export const handler = async (input: EnableTrustedAccessForServicesInput) => {
 
   const { accounts } = input;
 
+  const securityAccount = accounts.find(a => a.type === 'security');
+  if (!securityAccount) {
+    throw new Error(`Cannot find account with type security`);
+  }
+  const securityAccountId: string = securityAccount.id;
+
   const org = new Organizations();
   await org.enableAWSServiceAccess('ram.amazonaws.com');
   console.log('Enabled Resource Access Manager service access within the Organization.');
 
-  await org.enableAWSServiceAccess('access-analyzer.amazonaws.com');
-  console.log('Enabled Access Analyzer service access within the Organization.');
-
-  const securityAccountKey = 'security';
-  const securityAccount = accounts.find(a => a.key === securityAccountKey);
-  if (!securityAccount) {
-    throw new Error(`Cannot find account with key ${securityAccountKey}`);
-  }
-  const securityAccountId: string = securityAccount.id;
-  await org.registerDelegatedAdministrator(securityAccountId, 'access-analyzer.amazonaws.com');
-  console.log('Security account registered as delegated administrator for Access Analyzer in the organization.');
-
   await org.enableAWSServiceAccess('fms.amazonaws.com');
   console.log('Enabled Firewall Manager service access within the Organization.');
 
+  const fms = new FMS();
+  await fms.associateAdminAccount(securityAccountId);
+  console.log('Security account registered as admin account for Firewall Manager in the organization.');
+
+  await org.enableAWSServiceAccess('access-analyzer.amazonaws.com');
+  console.log('Enabled Access Analyzer service access within the Organization.');
+
   await org.registerDelegatedAdministrator(securityAccountId, 'access-analyzer.amazonaws.com');
-  console.log('Security account registered as delegated administrator for Firewall Manager in the organization.');
+  console.log('Security account registered as delegated administrator for Access Analyzer in the organization.');
 
   return {
     status: 'SUCCESS',
