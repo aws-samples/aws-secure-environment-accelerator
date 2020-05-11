7f88c7f86a6cf431986c066dcc50539e3b10066b
Gillis Van Ginderachter <ggindera@amazon.com>
2020-05-11 18:21:46 -0400

    Increase exponential backoff initial delay

--- common-lambda/lib/aws/backoff.ts
+++ common-lambda/lib/aws/backoff.ts
@@ -1,7 +1,11 @@
 import { backOff } from 'exponential-backoff';
 
+/**
+ * Auxiliary function to retry AWS SDK calls when a throttling error occurs.
+ */
 export function throttlingBackOff<T>(request: () => Promise<T>): Promise<T> {
   return backOff(request, {
+    startingDelay: 500,
     retry: e => e.errorType === 'TooManyRequestsException' || e.code === 'Throttling',
   });
 }
