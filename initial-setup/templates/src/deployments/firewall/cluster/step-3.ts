a7ce0e44fceaab7993d791c773577b31caefaae1
Gillis Van Ginderachter <ggindera@amazon.com>
2020-05-24 18:49:25 -0400

    Set firewall license and config file in firewall config

--- initial-setup/templates/src/deployments/firewall/cluster/step-3.ts
+++ initial-setup/templates/src/deployments/firewall/cluster/step-3.ts
@@ -1,10 +1,9 @@
-import * as path from 'path';
 import { pascalCase } from 'pascal-case';
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
-import * as s3deployment from '@aws-cdk/aws-s3-deployment';
 import * as c from '@aws-pbmm/common-lambda/lib/config';
 import { StackOutput } from '@aws-pbmm/common-lambda/lib/util/outputs';
+import { S3Template } from '@custom-resources/s3-template';
 import { Vpc } from '@aws-pbmm/constructs/lib/vpc';
 import { FirewallCluster, FirewallInstance } from '@aws-pbmm/constructs/lib/firewall';
 import { AccountStacks } from '../../../common/account-stacks';
@@ -15,6 +14,7 @@ import {
   FirewallInstanceOutput,
   FirewallInstanceOutputType,
 } from './outputs';
+import { createRoleName } from '@aws-pbmm/common-cdk/lib/core/accelerator-name-generator';
 
 export interface FirewallStep3Props {
   accountStacks: AccountStacks;
@@ -62,6 +62,7 @@ export async function step3(props: FirewallStep3Props) {
 
     const accountStack = accountStacks.getOrCreateAccountStack(accountKey);
     await createFirewallCluster({
+      config,
       scope: accountStack,
       vpc,
       firewallConfig,
@@ -74,42 +75,48 @@ export async function step3(props: FirewallStep3Props) {
  * Create firewall for the given VPC and config in the given scope.
  */
 async function createFirewallCluster(props: {
+  config: c.AcceleratorConfig,
   scope: cdk.Construct;
   vpc: Vpc;
   firewallConfig: c.FirewallConfig;
   firewallVpnConnections: FirewallVpnConnection[];
 }) {
-  const { scope, vpc, firewallConfig, firewallVpnConnections } = props;
+  const { config, scope, vpc, firewallConfig, firewallVpnConnections } = props;
 
   const securityGroup = vpc.findSecurityGroupByName(firewallConfig['security-group']);
 
   // TODO Single bucket per account!
   // This is for testing
-  const bucket = new s3.Bucket(scope, 'FirewallConfig', {
-    removalPolicy: cdk.RemovalPolicy.DESTROY,
+  const bucket = new s3.Bucket(scope, 'FirewallConfig');
+
+  const centralBucketName = config['global-options']['central-bucket'];
+  const centralBucket = s3.Bucket.fromBucketAttributes(scope, 'CentralBucket', {
+    bucketName: centralBucketName
   });
 
-  const artifactsFolderPath = path.join(__dirname, 'artifacts');
-  const artifactsDeployment = new s3deployment.BucketDeployment(scope, 'FirewallConfigDeploy', {
-    destinationBucket: bucket,
-    sources: [s3deployment.Source.asset(artifactsFolderPath)],
+  // Use S3 template to copy over the license from the central bucket
+  const copyLicense = new S3Template(scope, 'FirewallLicense', {
+    templateBucket: centralBucket,
+    templatePath: firewallConfig.license,
+    outputBucket: bucket,
+    outputPath: 'license.lic',
   });
 
   const cluster = new FirewallCluster(scope, 'Firewall', {
     vpcCidrBlock: vpc.cidrBlock,
     imageId: firewallConfig['image-id'],
     instanceType: firewallConfig['instance-sizes'],
+    roleName: createRoleName('Firewall'),
     configuration: {
       bucket,
       bucketRegion: cdk.Aws.REGION,
-      licensePath: 'fortigate.lic',
-      templateBucket: bucket,
-      templateConfigPath: 'fortigate.txt',
+      licensePath: 'license.lic',
+      templateBucket: centralBucket,
+      templateConfigPath: firewallConfig.config,
     },
   });
-
-  // Make sure the artifacts are deployed before using them
-  cluster.node.addDependency(artifactsDeployment);
+  // Make sure the license is copied before creating the cluster
+  cluster.node.addDependency(copyLicense);
 
   // Make sure the cluster can read the license and write the configuration template
   bucket.grantRead(cluster.instanceRole);
