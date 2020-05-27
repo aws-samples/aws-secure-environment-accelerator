c9a8bf6e2e2f0cec2b18093ebafbf79dfe11826d
Gillis Van Ginderachter <ggindera@amazon.com>
2020-05-27 17:10:12 -0400

    Improve codebuild-deploy ts-node speed

--- initial-setup/templates/codebuild-bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ASSUME_ROLE_PLUGIN_PATH="$(pwd)/../../plugins/assume-role"
-
-pnpx cdk bootstrap \
-  --plugin "$ASSUME_ROLE_PLUGIN_PATH" \
-  --app "pnpx ts-node src/app.ts"
--- initial-setup/templates/codebuild-deploy.sh
+++ initial-setup/templates/codebuild-deploy.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+ASSUME_ROLE_PLUGIN_PATH="$(pwd)/../../plugins/assume-role"
+
 if [[ -z "${ACCELERATOR_PHASE}" ]]; then
   echo "The environment variable ACCELERATOR_PHASE has to be set to the path of the app you want to deploy."
   exit 1
 fi
 
-echo "Deploying phase $ACCELERATOR_PHASE..."
+echo "Bootstrapping..."
 
-ASSUME_ROLE_PLUGIN_PATH="$(pwd)/../../plugins/assume-role"
+pnpx cdk bootstrap \
+  --plugin "$ASSUME_ROLE_PLUGIN_PATH" \
+  --app "pnpx ts-node --transpile-only src/app.ts"
+
+echo "Deploying phase $ACCELERATOR_PHASE..."
 
 pnpx cdk deploy "*" \
   --require-approval never \
@@ -16,4 +22,4 @@ pnpx cdk deploy "*" \
   --asset-metadata false \
   --force \
   --plugin "$ASSUME_ROLE_PLUGIN_PATH" \
-  --app "pnpx ts-node src/app.ts"
+  --app "pnpx ts-node --transpile-only src/app.ts"
