2cf1c406a0b9d27cba3c20b515ace1c0018dccce
Yao <longyao@amazon.com>
2020-06-05 15:16:36 -0400

    Initial commit for Organization Guard duty

---
+++ common-cdk/lib/organization/guardduty.ts
@@ -0,0 +1,49 @@
+import { Construct } from '@aws-cdk/core';
+import { CfnDetector, CfnMember, CfnMemberProps, CfnMaster, CfnMasterProps } from '@aws-cdk/aws-guardduty';
+
+
+export interface GuardDutyMasterProps{
+  /**
+   * All member account IDs used in the organization
+   */
+  memberProps: CfnMemberProps[];
+}
+
+/**
+ * Organization GuardDuty Construct, need to deployed to AWS Master Account of the Organization
+ */
+export class GuardDutyMaster extends Construct {
+  readonly detector: CfnDetector;
+
+  constructor(scope: Construct, id: string, props: GuardDutyMasterProps) {
+    super(scope, id);
+
+    this.detector = new CfnDetector(this, 'Detector', {
+      enable: true,
+    });
+
+    props.memberProps.map(memberProps => {
+      new CfnMember(this, `GuardDuty_Member_${memberProps.memberId}`, {
+        detectorId: this.detector.ref,
+        email: memberProps.email,
+        memberId: memberProps.memberId,
+        status: 'Invited',
+        ...memberProps
+      });
+    });
+  }
+}
+
+
+/**
+ * Organization GuardDuty Construct, need to deployed to AWS Member Account of the Organization
+ * Can only be deployed after GuardDutyMaster deployed to AWS Master Account already
+ *
+ */
+export class GuardDutyMember extends Construct {
+  constructor(scope: Construct, id:string, props: CfnMasterProps) {
+    super(scope, id);
+
+    new CfnMaster(this, `GuardDuty_Master_${props.masterId}`, props);
+  }
+}
---
+++ initial-setup/templates/src/deployments/organization/guardduty.ts
@@ -0,0 +1,65 @@
+import { AcceleratorConfig } from '@aws-pbmm/common-lambda/lib/config';
+import { AccountStacks } from '../../common/account-stacks';
+import { GuardDutyMaster, GuardDutyMember } from '@aws-pbmm/common-cdk/lib/organization';
+import { CfnMemberProps } from '@aws-cdk/aws-guardduty';
+import { Account, getAccountId } from '@aws-pbmm/common-outputs/lib/accounts';
+
+
+
+export interface GuardDutyStepProps {
+  accountStacks: AccountStacks;
+  config: AcceleratorConfig;
+  accounts: Account[];
+  detectorId: string;
+}
+
+/**
+ * First send guard duty invites to all member accounts in the organization
+ * 
+ * @param props accountStacks and config passed from phases
+ */
+export async function step1(props: GuardDutyStepProps) {
+  const masterAccountKey = props.config.getMandatoryAccountKey('master');
+  const masterAccountStack = props.accountStacks.getOrCreateAccountStack(masterAccountKey);
+
+  const memberProps: CfnMemberProps = [];
+
+  for (const [accountKey, accountConfig] of props.config.getAccountConfigs()) {
+    // only create member props so exclude master account
+    if(accountKey !== masterAccountKey) {
+      memberProps.push({
+        email: accountConfig.email,
+        memberId: getAccountId(props.accounts, accountKey),
+        detectorId: props.detectorId, // set to empty will be auto generated
+      })
+    }
+  }
+
+  const guardDutyMaster = new GuardDutyMaster(masterAccountStack, 'GuardDutyMaster', {
+    memberProps
+  });
+}
+
+/**
+ * Second accept all invites from master account in the organization
+ * 
+ * @param props accountStacks and config passed from phases
+ */
+export async function step2(props: GuardDutyStepProps) {
+  const masterAccountKey = props.config.getMandatoryAccountKey('master');
+
+  for (const [accountKey, accountConfig] of props.config.getAccountConfigs()) {
+    // only accept invites for member account excluding master account
+    if(accountKey !== masterAccountKey) {
+      const memberStack = props.accountStacks.getOrCreateAccountStack(accountKey);
+      
+      const memberAccountId = getAccountId(props.accounts, masterAccountKey);
+      if (memberAccountId) {
+        new GuardDutyMember(memberStack, 'GuardDutyMember', {
+          detectorId: props.detectorId,
+          masterId: memberAccountId,
+        })
+      }
+    }
+  }
+}
