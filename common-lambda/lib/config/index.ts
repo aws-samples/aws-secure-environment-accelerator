9ad3e7d78f85c3556acf8c2a3bcebb9474f88199
Naveen Koppula <43773714+naveenkoppula@users.noreply.github.com>
2020-06-17 21:02:17 +0530

    Feature/5.15b (#201)
    * Interm Commit
    
    * Prettier
    
    * temp
    
    * Checking desired value from most recent closed ticket
    
    * Considering Customer Confirm in place in config file for limits
    
    * Prettier
    
    * Update in cdk index
    
    * CWL Logging poart2
    
    * Prettier
    
    * Addin Supscription filter
    
    * Prettier
    
    * Fix for global exclusions per account
    
    * Fixxing Shallow Copy issue for globalExclusions
    
    * Prettier
    
    * Stop throwing error instead logging warn
    
    * Update on Review Comments
    
    * Adding CloudWatch Event rule to listen for new LogGroups
    
    * Prettier
    
    * Adding Update resource logic
    
    - Update happens for following scenarios
       -  change in exclusion list
       -  LogDestinationArn change
    
    * Moving CWL Central Logging to phase 1 and using existing central bucket
    
    * Prettier
    
    * Adding missed dependency
    
    * Per account Kinesis Firehose Stream as backup implementation
    
    * Lint fix
    
    * KMS Role policy changes
    
    * Removing Load organizations from this branch
    
    * Prettier
    
    * Changes based on Review
    
    - Changed Bucket policy with Kinesis Role restriction
    - Fixed update
    
    * Prettier

--- common-lambda/lib/config/index.ts
+++ common-lambda/lib/config/index.ts
@@ -553,6 +553,11 @@ export const SecurityHubFrameworksConfigType = t.interface({
   ),
 });
 
+export const CwlExclusions = t.interface({
+  account: t.string,
+  exclusions: t.array(t.string),
+});
+
 export const CentralServicesConfigType = t.interface({
   account: NonEmptyString,
   region: NonEmptyString,
@@ -564,6 +569,7 @@ export const CentralServicesConfigType = t.interface({
   'cwl-glbl-exclusions': optional(t.array(t.string)),
   'ssm-to-s3': optional(t.boolean),
   'ssm-to-cwl': optional(t.boolean),
+  'cwl-exclusions': optional(t.array(CwlExclusions)),
 });
 
 export const ScpsConfigType = t.interface({
