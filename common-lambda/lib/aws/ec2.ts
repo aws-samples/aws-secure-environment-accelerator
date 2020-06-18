4aa0466c88d1a2138757b270260e804f919d5e8f
Naveen Kumar <nkoppula@amazon.com>
2020-06-18 21:03:41 +0530

    Delete VPC

--- common-lambda/lib/aws/ec2.ts
+++ common-lambda/lib/aws/ec2.ts
@@ -4,7 +4,13 @@ import {
   EnableEbsEncryptionByDefaultResult,
   ModifyEbsDefaultKmsKeyIdRequest,
   ModifyEbsDefaultKmsKeyIdResult,
+  SubnetList,
+  VpcList,
 } from 'aws-sdk/clients/ec2';
+import * as ec2 from 'aws-sdk/clients/ec2';
+import { throttlingBackOff } from './backoff';
+import { listWithNextToken, listWithNextTokenGenerator } from './next-token';
+import { collectAsync } from '../util/generator';
 
 export class EC2 {
   private readonly client: aws.EC2;
@@ -37,5 +43,107 @@ export class EC2 {
       DryRun: dryRun,
     };
     return this.client.modifyEbsDefaultKmsKeyId(params).promise();
+  }
+
+  /**
+   * to get all VPCs
+   */
+  async describeDefaultVpcs(): Promise<VpcList | undefined> {
+    const vpcs = await throttlingBackOff(() =>
+      this.client
+        .describeVpcs({
+          Filters: [
+            {
+              Name: 'isDefault',
+              Values: ['true'],
+            },
+          ],
+        })
+        .promise(),
+    );
+    return vpcs.Vpcs;
+  }
+
+  /**
+   * to delete Subnet
+   */
+  async deleteSubnet(subnetId: string): Promise<void> {
+    await throttlingBackOff(() =>
+      this.client
+        .deleteSubnet({
+          SubnetId: subnetId,
+        })
+        .promise(),
+    );
+  }
+
+  /**
+   * to delete VPC
+   */
+  async deleteVpc(vpcId: string): Promise<void> {
+    await throttlingBackOff(() =>
+      this.client
+        .deleteVpc({
+          VpcId: vpcId,
+        })
+        .promise(),
+    );
+  }
+
+  /**
+   * to describe Internet Gateways
+   */
+  async describeInternetGatewaysByVpc(vpcIds: string[]): Promise<ec2.InternetGatewayList | undefined> {
+    const igws = await throttlingBackOff(() =>
+      this.client
+        .describeInternetGateways({
+          Filters: [
+            {
+              Name: 'attachment.vpc-id',
+              Values: vpcIds
+            }
+          ]
+        })
+        .promise(),
+    );
+    return igws.InternetGateways;
+  }
+
+  /**
+   * to detach VPC from Internet Gateway
+   */
+  async detachInternetGateway(vpcId: string, igwId: string): Promise<void> {
+    await throttlingBackOff(() =>
+      this.client
+        .detachInternetGateway({
+          InternetGatewayId: igwId,
+          VpcId: vpcId
+        })
+        .promise(),
+    );
+  }
+
+  /**
+   * to delete Internet Gateway
+   */
+  async deleteInternetGateway(igwId: string): Promise<void> {
+    await throttlingBackOff(() =>
+      this.client
+        .deleteInternetGateway({
+          InternetGatewayId: igwId,
+        })
+        .promise(),
+    );
+  }
+
+  /**
+   * Wrapper around AWS.EC2.describeSubnets.
+   */
+  async listSubnets(input: ec2.DescribeSubnetsRequest): Promise<ec2.SubnetList> {
+    return listWithNextToken<ec2.DescribeSubnetsRequest, ec2.DescribeSubnetsResult, ec2.Subnet>(
+      this.client.describeSubnets.bind(this.client),
+      r => r.Subnets!,
+      input,
+    );
   }
 }
