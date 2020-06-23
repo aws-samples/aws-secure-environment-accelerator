cfd29f5cffba0b962bf844a5d18d4a0213e8776c
Gillis Van Ginderachter <ggindera@amazon.com>
2020-06-23 16:30:26 -0400

    Enable CloudTrail in the organization

--- initial-setup/lambdas/src/enable-trusted-access-for-services-step.ts
+++ initial-setup/lambdas/src/enable-trusted-access-for-services-step.ts
@@ -52,6 +52,9 @@ export const handler = async (input: EnableTrustedAccessForServicesInput) => {
   await org.enableAWSServiceAccess('guardduty.amazonaws.com');
   console.log('Enabled Guard Duty service access within the Organization.');
 
+  await org.enableAWSServiceAccess('cloudtrail.amazonaws.com');
+  console.log('Enabled Cloud Trail service access within the Organization.');
+
   const iam = new IAM();
   // as access analyzer will be created in security account, creating service linked role specifically in master.
   try {
