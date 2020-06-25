cc6d0439372d2e0805434e45aaa87a56a4a0ae9b
Dave Liggat <dliggat@amazon.com>
2020-06-25 07:52:17 -0700

    Add bump prefix logic

--- accelerator/cdk/src/index.ts
+++ accelerator/cdk/src/index.ts
@@ -19,17 +19,22 @@ async function main() {
   const stateMachineExecutionRole = env.ACCELERATOR_STATE_MACHINE_ROLE_NAME || `${acceleratorPrefix}PipelineRole`;
 
   const configRepositoryName = env.CONFIG_REPOSITORY_NAME || `${acceleratorPrefix}Config-Repo`;
-  const configBranchName = env.CONFIG_BRANCH_NAME || 'master';
   const configFilePath = env.CONFIG_FILE_PATH || 'config.json';
   const configS3FileName = env.CONFIG_S3_KEY || `config.json`;
   const configS3Bucket =
     env.CONFIG_S3_BUCKET || `${acceleratorPrefix.toLowerCase()}${cdk.Aws.ACCOUNT_ID}-${cdk.Aws.REGION}-config`;
+
+  let configBranchName = env.CONFIG_BRANCH_NAME || 'master';
+  if (configBranchName.startsWith('v')) {
+    configBranchName = `bump/${configBranchName}`;
+  }
 
   const enablePrebuiltProject = 'ENABLE_PREBUILT_PROJECT' in env;
 
   console.log(`Found accelerator context:`);
   console.log(`  Name: ${acceleratorName}`);
   console.log(`  Prefix: ${acceleratorPrefix}`);
+  console.log(`  Branch: ${configBranchName}`);
   console.log(`  Code Commit Configuration: ${configFilePath} from ${configRepositoryName}`);
   console.log(`  Existing S3 Configuration: ${configS3FileName} from ${configS3Bucket}`);
 
