2856a13092fa428a1b16ba6f4d3be9b702aa2139
Yao <longyao@amazon.com>
2020-07-07 16:16:54 -0400

    add exception handler in macie lambda

--- custom-resources/macie-enable-admin/lib/index.ts
+++ custom-resources/macie-enable-admin/lib/index.ts
@@ -1,6 +1,8 @@
+import * as path from 'path';
 import * as cdk from '@aws-cdk/core';
-import * as custom from '@aws-cdk/custom-resources';
 import * as iam from '@aws-cdk/aws-iam';
+import * as lambda from '@aws-cdk/aws-lambda';
+import { HandlerProperties } from '@custom-resources/macie-enable-admin-lambda';
 
 export interface MacieEnableAdminProps {
   accountId: string;
@@ -11,37 +13,55 @@ export interface MacieEnableAdminProps {
  * Custom resource implementation that retrive IPs for a created DNS Endpoint.
  */
 export class MacieEnableAdmin extends cdk.Construct {
-  private readonly resource: custom.AwsCustomResource;
+  private readonly resource: cdk.CustomResource;
 
   constructor(scope: cdk.Construct, id: string, props: MacieEnableAdminProps) {
     super(scope, id);
-    const { accountId, clientToken } = props;
-
-    const physicalResourceId = custom.PhysicalResourceId.of('EnableOrganizationAdminAccount');
-    const onCreateOrUpdate: custom.AwsSdkCall = {
-      service: 'Macie2',
-      action: 'enableOrganizationAdminAccount',
-      physicalResourceId,
-      parameters: {
-        adminAccountId: accountId,
-        clientToken,
-      },
+    const handlerProperties: HandlerProperties = {
+      accountId: props.accountId,
     };
 
-    this.resource = new custom.AwsCustomResource(this, 'Resource', {
-      resourceType: 'Custom::MacieEnableAdmin',
-      onCreate: onCreateOrUpdate,
-      onUpdate: onCreateOrUpdate,
-      policy: custom.AwsCustomResourcePolicy.fromStatements([
-        new iam.PolicyStatement({
-          actions: ['macie2:EnableOrganizationAdminAccount'],
-          resources: ['*'],
-        }),
-        new iam.PolicyStatement({
-          actions: ['organizations:*'],
-          resources: ['*'],
-        }),
-      ]),
+    this.resource = new cdk.CustomResource(this, 'Resource', {
+      resourceType: 'Custom::MacieAdmin',
+      serviceToken: this.lambdaFunction.functionArn,
+      properties: handlerProperties,
+    });
+  }
+
+  private get lambdaFunction(): lambda.Function {
+    const constructName = `MacieAdminLambda`;
+    const stack = cdk.Stack.of(this);
+    const existing = stack.node.tryFindChild(constructName);
+    if (existing) {
+      return existing as lambda.Function;
+    }
+
+    const lambdaPath = require.resolve('@custom-resources/macie-enable-admin-lambda');
+    const lambdaDir = path.dirname(lambdaPath);
+
+    const role = new iam.Role(stack, `${constructName}Role`, {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    });
+
+    role.addToPrincipalPolicy(
+      new iam.PolicyStatement({
+        actions: ['organizations:*'],
+        resources: ['*'],
+      }),
+    );
+    role.addToPrincipalPolicy(
+      new iam.PolicyStatement({
+        actions: ['macie2:EnableOrganizationAdminAccount'],
+        resources: ['*'],
+      }),
+    );
+
+    return new lambda.Function(stack, constructName, {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset(lambdaDir),
+      handler: 'index.handler',
+      role,
+      timeout: cdk.Duration.seconds(10),
     });
   }
 }
