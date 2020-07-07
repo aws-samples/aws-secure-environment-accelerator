eed06301a163d016f233683af918c79d778cff78
Naveen Kumar <nkoppula@amazon.com>
2020-07-07 10:09:55 +0530

    Changing file name

--- common-cdk/lib/core/index.ts
+++ common-cdk/lib/core/index.ts
@@ -1,4 +1,4 @@
 export * from './cfn-json-include';
 export * from './cfn-yaml-include';
 export * from './name-tagger';
-export * from './accel-protected-tagger';
+export * from './accelerator-protected-tagger';
