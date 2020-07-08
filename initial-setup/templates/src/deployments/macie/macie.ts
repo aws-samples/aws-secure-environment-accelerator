23ead1f6874facbce545b32624af70cf6d6831d9
Yao <longyao@amazon.com>
2020-07-08 08:04:33 -0400

    add Enable Macie to step 1

--- initial-setup/templates/src/deployments/macie/macie.ts
+++ initial-setup/templates/src/deployments/macie/macie.ts
@@ -3,6 +3,7 @@ import { Account, getAccountId } from '@aws-pbmm/common-outputs/lib/accounts';
 import { AcceleratorConfig } from '@aws-pbmm/common-lambda/lib/config';
 import { MacieEnableAdmin } from '@custom-resources/macie-enable-admin';
 import { MacieCreateMember } from '@custom-resources/macie-create-member';
+import { MacieEnable, MacieFrequency, MacieStatus } from '@custom-resource/macie-enable';
 
 export interface MacieStepProps {
   accountStacks: AccountStacks;
@@ -14,6 +15,7 @@ export async function step1(props: MacieStepProps) {
   const { accountStacks, config, accounts } = props;
 
   const enableMacie = config['global-options']['central-security-services'].macie;
+  const frequency = config['global-options']['central-security-services']['macie-frequency'];
 
   // skipping Macie if not enabled
   if (!enableMacie) {
@@ -26,10 +28,23 @@ export async function step1(props: MacieStepProps) {
   const masterAccountId = getAccountId(accounts, masterAccountKey);
   const regions = config['global-options']['supported-regions'];
   regions?.map(region => {
-    // Guard duty need to be enabled from master account of the organization
+    // Macie need to be enabled from master account of the organization
     const masterAccountStack = accountStacks.getOrCreateAccountStack(masterOrgKey, region);
 
     if (masterAccountId) {
+      let findingPublishingFrequency = MacieFrequency.SIX_HOURS;
+      if (frequency === 6) {
+        findingPublishingFrequency = MacieFrequency.SIX_HOURS;
+      } else if (frequency === 1) {
+        findingPublishingFrequency = MacieFrequency.ONE_HOUR;
+      } else if (frequency === 15) {
+        findingPublishingFrequency = MacieFrequency.FIFTEEN_MINUTES;
+      }
+      const enable = new MacieEnable(masterAccountStack, 'MacieEnable', {
+        findingPublishingFrequency,
+        status: MacieStatus.ENABLE,
+      });
+
       const admin = new MacieEnableAdmin(masterAccountStack, 'MacieEnableAdmin', {
         accountId: masterAccountId,
       });
