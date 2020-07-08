86cb5509723feb1d82d181dcce9045093a472dbc
Yao <longyao@amazon.com>
2020-07-08 16:06:10 -0400

    add exception ignore for already enabled macie account

--- custom-resources/macie-enable/lambda/src/index.ts
+++ custom-resources/macie-enable/lambda/src/index.ts
@@ -6,10 +6,23 @@ import {
 } from 'aws-lambda';
 import { errorHandler } from '@custom-resources/cfn-response';
 
+export enum MacieFrequency {
+  FIFTEEN_MINUTES = 'FIFTEEN_MINUTES',
+  ONE_HOUR = 'ONE_HOUR',
+  SIX_HOURS = 'SIX_HOURS',
+}
+
+export enum MacieStatus {
+  ENABLED = 'ENABLED',
+  PAUSED = 'PAUSED',
+}
+
 const macie = new AWS.Macie2();
 
 export interface HandlerProperties {
-  accountId: string;
+  findingPublishingFrequency: MacieFrequency;
+  status: MacieStatus;
+  clientToken?: string;
 }
 
 export const handler = errorHandler(onEvent);
@@ -32,33 +45,30 @@ async function onEvent(event: CloudFormationCustomResourceEvent) {
 function getPhysicalId(event: CloudFormationCustomResourceEvent): string {
   const properties = (event.ResourceProperties as unknown) as HandlerProperties;
 
-  return `${properties.accountId}`;
+  return `${properties.findingPublishingFrequency}${properties.status}`;
 }
 
 async function onCreateOrUpdate(
   event: CloudFormationCustomResourceCreateEvent | CloudFormationCustomResourceUpdateEvent,
 ) {
   const properties = (event.ResourceProperties as unknown) as HandlerProperties;
-  const response = await enableOrgAdmin(properties);
+  const response = await enableMacie(properties);
   return {
     physicalResourceId: getPhysicalId(event),
     data: {},
   };
 }
 
-async function enableOrgAdmin(properties: HandlerProperties) {
+async function enableMacie(properties: HandlerProperties) {
   try {
     const enableAdmin = await macie
-      .enableOrganizationAdminAccount({
-        adminAccountId: properties.accountId,
-      })
-      .promise();
+      .enableMacie(properties).promise();
 
     return enableAdmin;
   } catch (e) {
     const message = `${e}`;
     if (
-      message.includes('The request failed because an account is already enabled as the Macie delegated administrator for the organization')
+      message.includes('Macie has already been enabled')
     ) {
       console.warn(e);
     } else {
--- custom-resources/macie-enable/lib/index.ts
+++ custom-resources/macie-enable/lib/index.ts
@@ -1,6 +1,9 @@
+import * as path from 'path';
 import * as cdk from '@aws-cdk/core';
 import * as custom from '@aws-cdk/custom-resources';
 import * as iam from '@aws-cdk/aws-iam';
+import * as lambda from '@aws-cdk/aws-lambda';
+import { HandlerProperties } from '@custom-resources/macie-enable-lambda';
 
 export enum MacieFrequency {
   FIFTEEN_MINUTES = 'FIFTEEN_MINUTES',
@@ -23,11 +26,19 @@ export interface MacieEnableProps {
  * Custom resource implementation that retrive IPs for a created DNS Endpoint.
  */
 export class MacieEnable extends cdk.Construct {
-  private readonly resource: custom.AwsCustomResource;
+  private readonly resource: cdk.CustomResource;
 
   constructor(scope: cdk.Construct, id: string, props: MacieEnableProps) {
     super(scope, id);
     const { findingPublishingFrequency, status, clientToken } = props;
+
+    const handlerProperties: HandlerProperties = props;
+
+    this.resource = new cdk.CustomResource(this, 'Resource', {
+      resourceType: 'Custom::MacieAdmin',
+      serviceToken: this.lambdaFunction.functionArn,
+      properties: handlerProperties,
+    });
 
     const physicalResourceId = custom.PhysicalResourceId.of('EnableMacie');
     const onCreateOrUpdate: custom.AwsSdkCall = {
@@ -41,22 +52,47 @@ export class MacieEnable extends cdk.Construct {
       },
     };
 
-    this.resource = new custom.AwsCustomResource(this, 'Resource', {
+    this.resource = new cdk.CustomResource(this, 'Resource', {
       resourceType: 'Custom::MacieEnable',
-      onCreate: onCreateOrUpdate,
-      onUpdate: onCreateOrUpdate,
-      policy: custom.AwsCustomResourcePolicy.fromStatements([
-        new iam.PolicyStatement({
-          actions: ['macie2:EnableMacie'],
-          resources: ['*'],
-        }),
-        // need this permission to create service linked role for macie
-        // see https://docs.aws.amazon.com/macie/latest/user/service-linked-roles.html
-        new iam.PolicyStatement({
-          actions: ['iam:CreateServiceLinkedRole'],
-          resources: ['*'],
-        }),
-      ]),
+      serviceToken: this.lambdaFunction.functionArn,
+      properties: handlerProperties,
+    });
+  }
+
+  private get lambdaFunction(): lambda.Function {
+    const constructName = `MacieAdminLambda`;
+    const stack = cdk.Stack.of(this);
+    const existing = stack.node.tryFindChild(constructName);
+    if (existing) {
+      return existing as lambda.Function;
+    }
+
+    const lambdaPath = require.resolve('@custom-resources/macie-enable-admin-lambda');
+    const lambdaDir = path.dirname(lambdaPath);
+
+    const role = new iam.Role(stack, `${constructName}Role`, {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    });
+
+    role.addToPrincipalPolicy(
+      new iam.PolicyStatement({
+        actions: ['iam:CreateServiceLinkedRole'],
+        resources: ['*'],
+      }),
+    );
+    role.addToPrincipalPolicy(
+      new iam.PolicyStatement({
+        actions: ['macie2:EnableMacie'],
+        resources: ['*'],
+      }),
+    );
+
+    return new lambda.Function(stack, constructName, {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset(lambdaDir),
+      handler: 'index.handler',
+      role,
+      timeout: cdk.Duration.seconds(10),
     });
   }
 }
