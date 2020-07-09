0defec6d89173ceb46f5a2caa4a401eaa74fcff3
Naveen Kumar <nkoppula@amazon.com>
2020-07-09 16:33:40 +0530

    Retriving failed state before publishing topic

--- initial-setup/lambdas/src/notify-statemachine-failure.ts
+++ initial-setup/lambdas/src/notify-statemachine-failure.ts
@@ -1,22 +1,53 @@
 import { SNS } from '@aws-pbmm/common-lambda/lib/aws/sns';
+import { StepFunctions } from '@aws-pbmm/common-lambda/lib/aws/stepfunctions';
 
 interface NotifyErrorInput {
   error: string;
   cause: string;
   notificationTopicArn: string;
+  executionId: string;
 }
 
 const sns = new SNS();
+const sfn = new StepFunctions();
 
 export const handler = async (input: NotifyErrorInput): Promise<string> => {
   console.log(`State Machine Execution Failed...`);
   console.log(JSON.stringify(input, null, 2));
-  const errorCause = JSON.parse(input.cause);
+
+  const { cause, executionId, notificationTopicArn } = input;
+  const errorCause = JSON.parse(cause);
+  try {
+    const failedState = await getFailedState(executionId);
+    errorCause.FailedState = failedState!;
+  } catch (error) {
+    console.error(error);
+  }
+  console.log(`Publishing Error to SNS Topic`);
+  console.log(JSON.stringify(errorCause, null, 2));
   await sns.publish({
     Message: JSON.stringify(errorCause),
-    TopicArn: input.notificationTopicArn,
+    TopicArn: notificationTopicArn,
     MessageStructure: 'email-json',
     Subject: `Accelerator State Machine Failure`,
   });
   return 'SUCCESS';
 };
+
+async function getFailedState(executionArn: string): Promise<string | undefined> {
+  const recentEvents = await sfn.getExecutionHistory({
+    executionArn,
+    reverseOrder: true,
+  });
+  const abortEvent = recentEvents.find(e => e.type === 'TaskStateAborted');
+  if (!abortEvent) {
+    return;
+  }
+  const abortEventId = abortEvent.id;
+  const checkEvents = recentEvents.filter(e => e.id < abortEventId);
+  const previousStartState = checkEvents.find(e => e.type === 'TaskStateEntered');
+  if (!previousStartState) {
+    return;
+  }
+  return previousStartState.stateEnteredEventDetails?.name;
+}
