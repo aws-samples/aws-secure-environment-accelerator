7f4aa9c71d27fc2c725517afc7b9ca79fcf76b3e
Yao <longyao@amazon.com>
2020-07-21 10:07:35 -0400

    reformat

--- initial-setup/lambdas/src/store-repo-info.ts
+++ initial-setup/lambdas/src/store-repo-info.ts
@@ -12,7 +12,7 @@ const ssm = new SSM();
 
 export const handler = async (input: StoreRepoInfoInput) => {
   console.log(`Storing Repo input...`);
-  const output = JSON.stringify(input, null, 2)
+  const output = JSON.stringify(input, null, 2);
   console.log(output);
 
   const param = await ssm.putParameter('/accelerator/version', output, 'String');
