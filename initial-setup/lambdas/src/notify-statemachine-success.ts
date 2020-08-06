9182bd2c8a983a2e6c920050cb2c55b452070378
Naveen Kumar <nkoppula@amazon.com>
2020-08-06 09:07:25 +0530

    Reoving Accounts if message length is grater than 256kb
    - Reoving Accounts if message length is grater than 256kb
    - Added allAccounts: No if json is trimmed.

--- initial-setup/lambdas/src/notify-statemachine-success.ts
+++ initial-setup/lambdas/src/notify-statemachine-success.ts
@@ -1,6 +1,9 @@
 import { SNS } from '@aws-pbmm/common-lambda/lib/aws/sns';
 import { Account } from '@aws-pbmm/common-outputs/lib/accounts';
 
+const MAX_SNS_PUBLISH_CHAR = 255500;
+const AVG_CHARS_PER_ACCOUNT = 100;
+
 interface NotifyErrorInput {
   notificationTopicArn: string;
   accounts: Account[];
@@ -13,12 +16,29 @@ export const handler = async (input: NotifyErrorInput): Promise<string> => {
   console.log(`State Machine Execution Success...`);
   console.log(JSON.stringify(input, null, 2));
   const { accounts, acceleratorVersion } = input;
-  const errorCause = {
+  const responseAccounts = accounts.map(acc => ({
+    key: acc.key,
+    id: acc.id,
+    ouPath: acc.ouPath,
+    name: acc.name,
+  }));
+  let errorCause = {
+    allAccounts: 'Yes',
     acceleratorVersion,
-    accounts,
+    accounts: responseAccounts,
   };
+  let errorCauseStr = JSON.stringify(errorCause);
+  while (errorCauseStr.length > MAX_SNS_PUBLISH_CHAR) {
+    const avgRemoveAccounts = Math.ceil((errorCauseStr.length - MAX_SNS_PUBLISH_CHAR) / AVG_CHARS_PER_ACCOUNT);
+    errorCause = {
+      allAccounts: 'No',
+      acceleratorVersion,
+      accounts: errorCause.accounts.slice(0, errorCause.accounts.length - avgRemoveAccounts),
+    };
+    errorCauseStr = JSON.stringify(errorCause);
+  }
   await sns.publish({
-    Message: JSON.stringify(errorCause),
+    Message: errorCauseStr,
     TopicArn: input.notificationTopicArn,
     MessageStructure: 'email-json',
     Subject: `Accelerator State Machine Execution Success`,
