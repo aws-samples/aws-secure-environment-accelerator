a289f5083a26ee95cc1e7cdeb53514e8d96350bd
Naveen Kumar <nkoppula@amazon.com>
2020-08-07 14:28:13 +0530

    Removing Handler

--- initial-setup/lambdas/src/notify-statemachine-failure.ts
+++ initial-setup/lambdas/src/notify-statemachine-failure.ts
@@ -34,7 +34,7 @@ export const handler = async (input: NotifyErrorInput): Promise<string> => {
     FailedState: failedState!,
     // Rest of the error
     ...errorCause,
-  }
+  };
 
   try {
     if (errorCauseReturn.Input) {
@@ -73,10 +73,3 @@ async function getFailedState(executionArn: string): Promise<string | undefined>
   }
   return previousStartState.stateEnteredEventDetails?.name;
 }
-handler({
-  "notificationTopicArn": "arn:aws:sns:ca-central-1:538235518685:PBMMAccel--MainStateMachine-Status_topic",
-  "acceleratorVersion": "1.1.4",
-  "executionId": "arn:aws:states:ca-central-1:538235518685:execution:PBMMAccel-MainStateMachine_sm:234d8a6a-c91b-4b78-aa9e-8a4a72346efb",
-  "cause": "{\"errorType\":\"FileDoesNotExistException\",\"errorMessage\":\"Could not find path raw/config.json\",\"trace\":[\"FileDoesNotExistException: Could not find path raw/config.json\",\"    at constructor.extractError (/var/task/index.js:125:6861)\",\"    at constructor.callListeners (/var/task/index.js:261:31814)\",\"    at constructor.emit (/var/task/index.js:261:31524)\",\"    at constructor.emitEvent (/var/task/index.js:314:556302)\",\"    at constructor.e (/var/task/index.js:314:551841)\",\"    at r.runTo (/var/task/index.js:314:558144)\",\"    at /var/task/index.js:314:558350\",\"    at constructor.<anonymous> (/var/task/index.js:314:552111)\",\"    at constructor.<anonymous> (/var/task/index.js:314:556358)\",\"    at constructor.callListeners (/var/task/index.js:261:31920)\"]}",
-  "error": "FileDoesNotExistException"
-})
