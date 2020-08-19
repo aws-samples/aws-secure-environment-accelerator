43872276ea24f11aef571b59a719313d52a059cf
nachundu <nachundu@amazon.com>
2020-08-19 12:48:29 +0530

    added comments

--- reference-artifacts/upgradev117-cleanup/resolver-rule-cleanup.sh
+++ reference-artifacts/upgradev117-cleanup/resolver-rule-cleanup.sh
@@ -3,24 +3,28 @@
 # Domain name to search for resolver rule
 Domain="dept.cloud-nuage.gc.ca"
 
+# Finds the resolver rule Id for the given domain name
 function get_resolver_id() {
     resolver_id=$(aws route53resolver list-resolver-rules --filters Name=DomainName,Values=$Domain --query ResolverRules[].Id --output text)
 }
 
+# Finds VPCs associated to the resolver rule Id
 function get_vpc_ids() {
     vpc_ids=$(aws route53resolver list-resolver-rule-associations --filters Name=ResolverRuleId,Values=$1 --query ResolverRuleAssociations[].VPCId --output json | awk '{print $1}' | tr -d '[]",')
 }
 
+# Checks association of VPCs from the resolver rule
 function _checkStatus() {
     while [ -n "$vpc_ids" ]
         do
-        echo "waiting to disassociate VPCs from Resolver rule $resolver_id"
+        echo "waiting to disassociate VPCs from resolver rule $resolver_id"
         sleep 5
         get_vpc_ids $resolver_id
     done
-    echo "completed disassociating VPCs from Resolver rule $resolver_id"
+    echo "completed disassociating VPCs from resolver rule $resolver_id"
 }
 
+# Verify and disassicate VPCs from resolver rule
 function disassociate_vpc_ids() {
     get_resolver_id
     if [ -z "$resolver_id" ]
@@ -31,9 +35,9 @@ function disassociate_vpc_ids() {
         get_vpc_ids $resolver_id
         if [ -z "$vpc_ids" ] 
             then
-            echo "VPCs are not associated to Resolver rule $resolver_id"
+            echo "VPCs are not associated to resolver rule $resolver_id"
         else
-            echo "started disassociating VPCs from Resolver rule $resolver_id"
+            echo "started disassociating VPCs from resolver rule $resolver_id"
             for vpc_id in $vpc_ids; do
                 result=$(aws route53resolver disassociate-resolver-rule --resolver-rule-id ${resolver_id} --vpc-id $vpc_id)
             done
