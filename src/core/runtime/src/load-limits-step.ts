35f22fb260753ffd5e84c96a41dbaf7bd005a038
Naveen Kumar <nkoppula@amazon.com>
2020-09-30 16:32:34 +0530

    Fixing tests

--- src/core/runtime/src/load-limits-step.ts
+++ src/core/runtime/src/load-limits-step.ts
@@ -70,7 +70,7 @@ export const handler = async (input: LoadLimitsInput) => {
     commitId: configCommitId,
   });
 
-  const defaultRegion: string = config['global-options']['aws-org-master'].region!;
+  const defaultRegion: string = config['global-options']['aws-org-master'].region;
 
   // Capture limit results
   const limits: LimitOutput[] = [];
