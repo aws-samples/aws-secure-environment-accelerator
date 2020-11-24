03c639d4e2eb34e45ffa5fd230ea97d8e9712a81
Brian969 <56414362+Brian969@users.noreply.github.com>
2020-11-24 12:06:53 -0500

    tweak GD role permissions

--- src/deployments/cdk/src/deployments/iam/guardduty-roles.ts
+++ src/deployments/cdk/src/deployments/iam/guardduty-roles.ts
@@ -73,7 +73,7 @@ export async function createAdminSetupRole(stack: AccountStack) {
         'guardduty:CreateMembers',
         'guardduty:UpdateOrganizationConfiguration',
         'guardduty:DescribeOrganizationConfiguration',
-        'guardduty:UpdateMemberDetectors',
+        'guardduty:UpdateDetector',
         'guardduty:DeleteMembers',
       ],
       resources: ['*'],
