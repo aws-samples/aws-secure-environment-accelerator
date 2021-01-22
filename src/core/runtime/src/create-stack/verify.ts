6e5fe96dfc1c68aac42d83560559c0a784601cf3
Naveen Koppula <43773714+naveenkoppula@users.noreply.github.com>
2021-01-22 18:31:41 +0530

    fix(core): Removing StackSet for installing Pipeline role in Org Accounts (#568)
    * Creating Accelerator Execution Role in all accounts
    * Adding Cloudformation service to assume pipelinerole
    * Setting maxConcurrency for installing pipeline roles to 40

--- src/core/runtime/src/create-stack/verify.ts
+++ src/core/runtime/src/create-stack/verify.ts
@@ -1,19 +1,29 @@
 import { CloudFormation } from '@aws-accelerator/common/src/aws/cloudformation';
+import { STS } from '@aws-accelerator/common/src/aws/sts';
 
 const SUCCESS_STATUSES = ['CREATE_COMPLETE', 'UPDATE_COMPLETE'];
 const FAILED_STATUSES = ['CREATE_FAILED', 'ROLLBACK_COMPLETE', 'ROLLBACK_FAILED', 'UPDATE_ROLLBACK_COMPLETE'];
 
 interface CheckStepInput {
   stackName?: string;
+  accountId?: string;
+  assumedRoleName?: string;
 }
 
 export const handler = async (input: Partial<CheckStepInput>) => {
   console.log(`Verifying stack with parameters ${JSON.stringify(input, null, 2)}`);
 
-  const { stackName } = input;
+  const { stackName, accountId, assumedRoleName } = input;
 
   // Deploy the stack using the assumed role in the current region
-  const cfn = new CloudFormation();
+  let cfn: CloudFormation;
+  if (accountId && assumedRoleName) {
+    const sts = new STS();
+    const credentials = await sts.getCredentialsForAccountAndRole(accountId, assumedRoleName);
+    cfn = new CloudFormation(credentials);
+  } else {
+    cfn = new CloudFormation();
+  }
   const stack = await cfn.describeStack(stackName!);
   if (!stack) {
     return {
