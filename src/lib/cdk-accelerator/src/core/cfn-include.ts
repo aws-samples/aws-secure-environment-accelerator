fe96764653abaad060b721755e63761cde76d60b
Naveen Kumar <nkoppula@amazon.com>
2021-02-01 17:21:15 +0530

    Fixing cfn-include merge

--- src/lib/cdk-accelerator/src/core/cfn-include.ts
+++ src/lib/cdk-accelerator/src/core/cfn-include.ts
@@ -1,14 +1,14 @@
 import * as cdk from '@aws-cdk/core';
 import * as cfn_inc from '@aws-cdk/cloudformation-include';
 
-export namespace CfnJsonInclude {
+export namespace CfnInclude {
   export interface Props {
     path: string;
   }
 }
 
-export class CfnJsonInclude extends cdk.Construct {
-  constructor(scope: cdk.Construct, id: string, props: CfnJsonInclude.Props) {
+export class CfnInclude extends cdk.Construct {
+  constructor(scope: cdk.Construct, id: string, props: CfnInclude.Props) {
     super(scope, id);
 
     new cfn_inc.CfnInclude(this, 'include', {
