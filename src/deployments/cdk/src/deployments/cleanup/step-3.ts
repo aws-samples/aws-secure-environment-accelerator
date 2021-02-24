7f88d4e244c54fc7114f9ca24eaee60b3950c35b
Naveen Kumar <nkoppula@amazon.com>
2021-02-24 13:42:32 +0530

    cleanup version check change

--- src/deployments/cdk/src/deployments/cleanup/step-3.ts
+++ src/deployments/cdk/src/deployments/cleanup/step-3.ts
@@ -24,7 +24,7 @@ export async function step3(props: CdkStackCleanupProps) {
 
   const installerVersion = context.installerVersion;
   // TODO verify the version and update it accordingly
-  const isCleanupRequired = sv.clean(installerVersion) === null ? true : sv.lt(installerVersion, '1.2.6');
+  const isCleanupRequired = sv.clean(installerVersion) === null ? true : sv.lt(installerVersion, '1.3.0');
   if (!isCleanupRequired) {
     return;
   }
