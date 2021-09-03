b45d9a802b8700ca9de46cee60c636155964c638
Adam McLean <24688110+apmclean@users.noreply.github.com>
2021-09-03 14:43:44 -0400

    Includes the ability to declare static IP Addresses for the Fortinet firewall ports within each attached subnet. (#796)
    Where static IP addresses are not declared in the configuration file IPs will be assigned automatically during instance creation (matching historical functionality).
    Includes code changes and an addition to the FAQ with details on use.

--- src/lib/cdk-constructs/src/firewall/instance.ts
+++ src/lib/cdk-constructs/src/firewall/instance.ts
@@ -123,11 +123,20 @@ export class FirewallInstance extends cdk.Construct {
     name: string;
     securityGroup: SecurityGroup;
     subnet: Subnet;
+    privateStaticIp?: string;
     eipAllocationId?: string;
     vpnTunnelOptions?: FirewallVpnTunnelOptions;
     additionalReplacements?: { [key: string]: string };
   }): ec2.CfnNetworkInterface {
-    const { name, securityGroup, subnet, eipAllocationId, vpnTunnelOptions, additionalReplacements } = props;
+    const {
+      name,
+      securityGroup,
+      subnet,
+      privateStaticIp,
+      eipAllocationId,
+      vpnTunnelOptions,
+      additionalReplacements,
+    } = props;
     const index = this.networkInterfacesProps.length;
 
     // Create network interface
@@ -135,6 +144,7 @@ export class FirewallInstance extends cdk.Construct {
       groupSet: [securityGroup.id],
       subnetId: subnet.id,
       sourceDestCheck: false,
+      privateIpAddress: privateStaticIp,
     });
     this.networkInterfacesProps.push({
       deviceIndex: `${index}`,
