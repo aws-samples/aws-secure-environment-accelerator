75a0d276c5ae96d88bb804506639d3d00afd7c0d
Ryan Jaeger <41204211+rjjaegeraws@users.noreply.github.com>
2022-02-08 13:56:07 -0800

    feature: SecurityHub findings to CloudWatch Logs (#867)
    - Send SH findings to CWL
    - Enabled whenever SH enabled in config file (no new params)
    - deals with log group already exists (if stack rolls back/retries)
    
    Co-authored-by: Brian969 <56414362+Brian969@users.noreply.github.com>

--- src/deployments/runtime/src/index.ts
+++ src/deployments/runtime/src/index.ts
@@ -17,5 +17,6 @@ export { handler as albIpMonitor } from './alb-to-alb-target/alb-ip-monitor';
 export { handler as albTargetRecordMonitor } from './alb-to-alb-target/alb-target-record-monitor';
 export { handler as geoIpDownloader } from './opensearch-siem/geo-ip-downloader';
 export { handler as firehoseCustomPrefix } from './firehose-custom-prefix/process-record';
+export { handler as eventToCWLPublisher } from './event-publish-cloudwatch-logs';
 import * as ouValidationEvents from './ou-validation-events';
 export { ouValidationEvents };
