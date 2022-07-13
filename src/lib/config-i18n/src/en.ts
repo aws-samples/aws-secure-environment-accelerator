9f5164f741cfc0edb6f1d1b785b799b37c72498a
Brian969 <56414362+Brian969@users.noreply.github.com>
2022-07-12 20:51:36 -0400

    tweak docs

--- src/lib/config-i18n/src/en.ts
+++ src/lib/config-i18n/src/en.ts
@@ -601,15 +601,15 @@ translate(c.RouteConfig, {
     target: {
       title: '',
       description:
-        'The target type for the next-hop, includes TGW, IGW, VGW, pcx, NATGW_subnet_azX, NFW_subnet_azX, s3, DynamoDB, and customer. If target is set to customer, the target-id and type are required.',
+        'The target type for the next-hop, includes TGW, IGW, VGW, pcx, GWLB, NATGW_subnet_azX, NFW_subnet_azX, s3, DynamoDB, and customer. If target is set to customer, the target-id and type are required.',
     },
     name: {
       title: '',
-      description: 'Route table name.',
+      description: 'Name of the GWLB to be targeted.',
     },
     az: {
       title: '',
-      description: 'Availability Zone (only used when targeting EC2 based firewall appliances)',
+      description: 'Availability Zone (only used when targeting GWLB endpoints and EC2 based firewall appliances)',
     },
     port: {
       title: '',
@@ -1132,15 +1132,15 @@ translate(c.IamRoleConfigType, {
     },
     'ssm-log-archive-write-access': {
       title: '',
-      description: 'Set to true if this role requires read and write access to the log archive bucket.',
+      description: 'Set to true to grant this role write access to the log archive buckets.',
     },
     'ssm-log-archive-read-only-access': {
       title: '',
-      description: 'Set to true if this role requires read-only access to the log archive bucket.',
+      description: 'Set to true to grant this role read-only access to the log archive buckets.',
     },
     'meta-data-read-only-access': {
       title: '',
-      description: 'Set to true if this role requires read only access to the metadata bucket',
+      description: 'Set to true to grant this role read-only access to the metadata bucket in the log archive account.',
     },
   },
 });
@@ -3319,7 +3319,8 @@ translate(c.GlobalOptionsConfigType, {
     },
     'meta-data-collection': {
       title: 'Metadata Collection',
-      description: 'Metadata collection service that stores information about ASEA in the management account',
+      description:
+        'Enables metadata collection service that stores information about ASEA configuration in the metadata bucket in the log archive account.',
     },
     'default-s3-retention': {
       title: 'Default account S3 logging bucket retention period',
