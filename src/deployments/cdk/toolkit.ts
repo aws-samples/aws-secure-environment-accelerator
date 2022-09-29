97e18c70b43b47debaf2553604368d2d509ba8ed
hickeydh-aws <hickeydh@amazon.com>
2022-09-29 09:35:13 -0500

    updated version

--- src/deployments/cdk/toolkit.ts
+++ src/deployments/cdk/toolkit.ts
@@ -21,7 +21,7 @@ import { Bootstrapper } from 'aws-cdk/lib/api/bootstrap';
 import { Command, Configuration } from 'aws-cdk/lib/settings';
 import { SdkProvider } from 'aws-cdk/lib/api/aws-auth';
 import { CloudFormationDeployments } from 'aws-cdk/lib/api/cloudformation-deployments';
-import { PluginHost } from 'aws-cdk/lib/plugin';
+import { PluginHost } from 'aws-cdk/lib/api/plugin';
 import { debugModeEnabled } from '@aws-cdk/core/lib/debug';
 import { AssumeProfilePlugin } from '@aws-accelerator/cdk-plugin-assume-role/src/assume-role-plugin';
 import { fulfillAll } from './promise';
