50cca476af944e6c0726938dd0e90b071375c3f5
Ryan Cerrato <rycerrat@amazon.com>
2023-02-20 13:47:28 -0600

    Fix for PrincipalArn behavior in L-SFN-MasterRole

--- src/core/cdk/src/initial-setup.ts
+++ src/core/cdk/src/initial-setup.ts
@@ -189,15 +189,18 @@ export namespace InitialSetup {
       // This is the maximum time before a build times out
       // The role used by the build should allow this session duration
       const buildTimeout = cdk.Duration.hours(4);
+      const roleName = createRoleName('L-SFN-MasterRole');
+      const roleArn = `arn:aws:${stack.partition}::${stack.account}role/${roleName}`;
 
       // The pipeline stage `InstallRoles` will allow the pipeline role to assume a role in the sub accounts
       const pipelineRole = new iam.Role(this, 'Role', {
-        roleName: createRoleName('L-SFN-MasterRole'),
+        roleName: roleName,
         assumedBy: new iam.CompositePrincipal(
           // TODO Only add root role for development environments
           new iam.ServicePrincipal('codebuild.amazonaws.com'),
           new iam.ServicePrincipal('lambda.amazonaws.com'),
           new iam.ServicePrincipal('events.amazonaws.com'),
+          new iam.ArnPrincipal(roleArn),
         ),
         managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess')],
         maxSessionDuration: buildTimeout,
