ca34616b73f3fdc8e28e13cd80f3fde435ba4a22
Ryan Cerrato <rycerrat@amazon.com>
2023-07-17 13:55:06 -0500

    Adding assets for custom config rules, updating convert config and resource mapping to fix issues found during testing. Also adding throttlingBackoff for detectdrift calls to ensure proper results are returned

--- reference-artifacts/Custom-Scripts/Pre-migration/src/common/utils/naming.ts
+++ reference-artifacts/Custom-Scripts/Pre-migration/src/common/utils/naming.ts
@@ -1,7 +1,6 @@
-import { name } from "aws-sdk/clients/importexport";
-
 // Subnets in ASEA are named as ${subnetName}_${vpcName}_az${subnetDefinition.az}_net
-export const createSubnetName = (vpcName: string, subnetName: string, az: string) => `${subnetName}_${vpcName}_az${az}_net`;
+export const createSubnetName = (vpcName: string, subnetName: string, az: string) =>
+  `${subnetName}_${vpcName}_az${az}_net`;
 
 export const createNatGatewayName = (subnetName: string, az: string) => `NATGW_${subnetName}_${az}_natgw`;
 
@@ -15,10 +14,26 @@ export const createTgwAttachName = (vpcName: string, tgwName: string) => `${vpcN
 
 export const createVpcName = (vpcName: string, suffix?: string) => `${vpcName}_vpc${suffix || ''}`;
 
-export const nfwRouteName = (routeTableName: string, destination: string) => destination === '0.0.0.0/0'? `${routeTableName}_nfw_route`: `${routeTableName}_nfw_${destination}_route`;
+export const nfwRouteName = (routeTableName: string, destination: string) =>
+  destination === '0.0.0.0/0' ? `${routeTableName}_nfw_route` : `${routeTableName}_nfw_${destination}_route`;
 
 export const transitGatewayName = (name: string) => `${name}_tgw`;
 
 export const transitGatewayPeerName = (sourceTgw: string, targetTgw: string) => `${sourceTgw}_to${targetTgw}_peer`;
 
-export const peeringConnectionName = (source: name, target: name) => `${source}-${target}_pcx`;
+export const transitGatewayRouteTableName = (name: string, tgwName: string) =>
+  `${transitGatewayName(tgwName)}_${name}_rt`;
+
+export const peeringConnectionName = (source: string, target: string) => `${source}-${target}_pcx`;
+
+export const securityGroupName = (name: string) => `${name}_sg`;
+
+export const createRouteTableName = (name: string) => `${name}_rt`;
+
+export const createNetworkFirewallPolicyName = (policyName: string, firewallName: string, prefix: string) =>
+  `${prefix}${firewallName}-${policyName}`;
+
+export const createNetworkFirewallName = (firewallName: string, prefix: string) => `${prefix}${firewallName}`;
+
+export const createNetworkFirewallRuleGroupName = (groupName: string, firewallName: string, prefix: string) =>
+  `${prefix}${firewallName}-${groupName}`;
