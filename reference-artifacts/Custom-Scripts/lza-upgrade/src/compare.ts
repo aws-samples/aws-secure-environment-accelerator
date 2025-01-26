18222155bdd4c08cb82b753ab79eee958b7c7ca3
hickeydh-aws <88673813+hickeydh-aws@users.noreply.github.com>
2025-01-26 10:13:59 -0600

    fixed compare readability

--- reference-artifacts/Custom-Scripts/lza-upgrade/src/compare.ts
+++ reference-artifacts/Custom-Scripts/lza-upgrade/src/compare.ts
@@ -2,19 +2,18 @@
 import { readFile, writeFile } from 'fs/promises';
 
 export abstract class Compare {
-    static async compareMappings(newMappingPath: string, originalMappingPath: string) {
-        const newMappingString = await readFile(newMappingPath, 'utf8');
+    static async compareMappings(originalMappingPath: string, newMappingPath: string) {
         const originalMappingString = await readFile(originalMappingPath, 'utf8');
+        const newMappingString = await readFile(newMappingPath, 'utf8');
         
-        const newMapping = JSON.parse(newMappingString);
         const originalMapping = JSON.parse(originalMappingString);
+        const newMapping = JSON.parse(newMappingString);
 
         const newMappingKeys = Object.keys(newMapping);
-        const originalMappingKeys = Object.keys(originalMapping);
-        const missingKeys = originalMappingKeys.filter(key => !newMappingKeys.includes(key));
-        const missingMappings = missingKeys.reduce((acc: any, key) => {
-            acc[key] = originalMapping[key];
-            return acc;
+        const missingKeys = newMappingKeys.filter((key) => !originalMapping[key]);
+        const missingMappings = missingKeys.reduce((acc: any, key: string) => {
+            acc[key] = newMapping[key];
+            return acc
         }, {});
         await writeFile('./outputs/missing-mappings.json', JSON.stringify(missingMappings, null, 2));
         console.log(`Missing mappings written to ./outputs/missing-mappings.json`);
